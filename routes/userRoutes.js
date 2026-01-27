fff80a9610aab92c001ac2288a8c79d8bbd8f1ef
tahirta143 <mtahirmusman2@gmail.com>
2026-01-28 00:04:59 +0500

    errors remove

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getProfile, updateProfile } = require("../controllers/userController");
-const { protectUser } = require("../middleware/authMiddleware");
+const { protectUser } = require("../middleware/authmiddleware");
 
 router.route("/profile")
   .get(protectUser, getProfile)
